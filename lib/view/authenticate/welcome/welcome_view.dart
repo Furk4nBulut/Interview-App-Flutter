28e70924aa93e5f9f9c5abe6a636b1614cbfd86b
FurkanBlt <furkanbtng@gmail.com>
2024-07-25 13:41:48 +0300

    basicUI changes Welcome Screen

--- lib/view/authenticate/welcome/welcome_view.dart
+++ lib/view/authenticate/welcome/welcome_view.dart
@@ -37,20 +37,20 @@ class WelcomeView extends StatelessWidget {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const SizedBox(height: 500.0),
+                          const SizedBox(height: 485.0),
                           RichText(
                             textAlign: TextAlign.center,
                             text: TextSpan(
                               children: [
                                 TextSpan(
-                                  text: 'Interview App\n',
+                                  text: 'Mülakat Uygulaması\n',
                                   style: TextStyle(
                                     fontSize: 50.0,
                                     fontWeight: FontWeight.w900,
-                                    color: Colors.white,
+                                    color: basarsoft_color,
                                     shadows: [
                                       Shadow(
-                                        color: basarsoft_color,
+                                        color: Colors.white.withOpacity(0.3),
                                         offset: Offset(2, 2),
                                         blurRadius: 3,
                                       ),
@@ -58,14 +58,14 @@ class WelcomeView extends StatelessWidget {
                                   ),
                                 ),
                                 TextSpan(
-                                  text: '\nMülakat Uygulaması',
+                                  text: '\nOnline mülakat uygulaması',
                                   style: TextStyle(
                                     fontSize: 20,
                                     fontWeight: FontWeight.w600,
                                     color: Colors.white,
                                     shadows: [
                                       Shadow(
-                                        color: basarsoft_color,
+                                        color: basarsoft_color.withOpacity(0.3),
                                         offset: Offset(2, 2),
                                         blurRadius: 3,
                                       ),
@@ -129,3 +129,5 @@ class WelcomeView extends StatelessWidget {
   }
 }
 
+const basarsoft_color = Color(0xFF02205C);
+const basarsoft_color_light = Color(0xFF00A5FF);
